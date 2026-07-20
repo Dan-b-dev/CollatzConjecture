e5e0f62b0687e74248c5b0c3336526e02ee1e385
Daniil Beri <beri.danil@gmail.com>
2026-07-20 17:30:57 +0200

    implemented overflow detection, early stoppage, removed expensive struct, strictly verification of peaks now

--- Collatz_algorithm.c
+++ Collatz_algorithm.c
@@ -2,69 +2,51 @@
 #include <stdint.h>
 #include <time.h>
 #include <inttypes.h>
-#define DOMAIN_UPPER_BOUND 10000000 // upper bound of positive integer inputs into the function
+#define DOMAIN_UPPER_BOUND 100000000 // upper bound of positive integer inputs into the function
 
-typedef struct {
-    uint64_t initial_value;
-    uint64_t stopping_time;
-    uint64_t peak;
-} CollatzData; 
+bool overflow_detected (uint64_t peak) {
+    return (peak > (UINT64_MAX - 1) / 3);
+}
 
-CollatzData collatz_data[DOMAIN_UPPER_BOUND];
-
-void compute_collatz(uint64_t n, uint64_t *stopping_time, uint64_t *peak) {
-    *stopping_time = 0;
+void compute_collatz(uint64_t n, uint64_t *peak) {
     *peak = n;
+    uint64_t initial_value = n;
     while (n != 1) {
-        if ( n % 2 == 1) {
+        if (overflow_detected(n) == 1) {
+            printf("overflow at %" PRIu64, n);
+            break;
+        }
+        if (n % 2 == 1) {
             n = 3 * n + 1;
         }
         else {
             n /= 2;
         }
-        (*stopping_time)++;
         if (n > *peak) {
             *peak = n;
         }
-    }
-}
-
-void build_collatz_data (void) {
-    uint64_t stopping_time;
-    uint64_t peak;
-    for (uint64_t i = 1; i <= DOMAIN_UPPER_BOUND; i++) {
-        compute_collatz(i, &stopping_time, &peak);
-        collatz_data[i].initial_value = i;
-        collatz_data[i].peak = peak;
-        collatz_data[i].stopping_time = stopping_time;
-    }
-}
-
-void find_peak_records () {
-    uint64_t current_record = 0;
-    for (uint64_t i = 1; i < DOMAIN_UPPER_BOUND; i++) {
-        if (collatz_data[i].peak > current_record ) {
-            current_record = collatz_data[i].peak; 
-            printf("index: %" PRIu64 " record: %" PRIu64 "\n", collatz_data[i].initial_value, current_record);
-          
+        if (n < initial_value) {
+            break;
         }
     }
 }
 
-void find_stopping_time_records () {
-    uint64_t current_record = 0;
-    for (uint64_t i = 1; i < DOMAIN_UPPER_BOUND; i++) {
-        if (collatz_data[i].stopping_time > current_record) {
-            current_record = collatz_data[i].stopping_time; 
-            printf("index: %" PRIu64 " record: %" PRIu64 "\n", collatz_data[i].initial_value, current_record);
-        }
+void find_peak_record (uint64_t initial_value, uint64_t *peak, uint64_t *current_record) {;
+    if (*peak > *current_record ) {
+        *current_record = *peak; 
+        printf("index: %" PRIu64 " record: %" PRIu64 "\n", initial_value, *current_record);
     }
+    
 }
 
 int main(void) {
+    uint64_t current_record = 0;
+    uint64_t peak;
     clock_t start = clock();
-    build_collatz_data();
-    find_peak_records();
+    for (uint64_t initial_value = 1; initial_value <= DOMAIN_UPPER_BOUND; initial_value++) {
+        compute_collatz(initial_value, &peak);
+        find_peak_record(initial_value, &peak, &current_record);
+    }
     clock_t end = clock();
     double seconds = (double)(end - start) / CLOCKS_PER_SEC;
     printf("\nRuntime: %.6f seconds\n", seconds);
